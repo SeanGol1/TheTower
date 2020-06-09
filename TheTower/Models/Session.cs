29347aea577d4ab9dfdbd1c668a8f3c56cbadc67
Sean Golden <S00154367@mail.itsligo.ie>
2020-06-09 22:12:00 +0100

    2 pages HTML updated

---
+++ TheTower/Models/Session.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TheTower.Models
+{
+    public class Session
+    {
+        public int ID { get; set; }
+        public ICollection<RoomNumber> LevelSetup { get; set; }
+
+    }
+}
---
+++ TheTower/Models/TowerContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TheTower.Models
+{
+    public class TowerContext : DbContext
+    {
+        public TowerContext()
+        {
+
+        }
+       
+        public DbSet<Biom> Biom { get; set; }
+        public DbSet<Event> Event{ get; set; }
+        public DbSet<Item> Item { get; set; }
+        public DbSet<Monster> Monster { get; set; }
+        public DbSet<MonsterCR> MonsterCR { get; set; }
+        public DbSet<Monster> RoomEvent { get; set; }
+        public DbSet<Item> RoomNumber { get; set; }
+        public DbSet<Monster> Session { get; set; }
+
+    }
+}
